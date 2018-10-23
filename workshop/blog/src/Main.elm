4c7581114e918abf519109572b8b09badf10b976
Manne Busk <mannebusk@gmail.com>
2018-10-23 13:48:32 +0200

    Added finished version of blog application

--- workshop/blog/src/Main.elm
+++ workshop/blog/src/Main.elm
@@ -1,7 +1,28 @@
 module Main exposing (main)
 
-import Html exposing (..)
+import Browser exposing (Document, UrlRequest(..))
+import Browser.Navigation as Nav
+import Url exposing (Url)
+import Html exposing (Html, a, div, text, h1, h2)
 import Html.Attributes exposing (href, style)
+
+
+type Msg
+  = UrlRequested UrlRequest
+  | UrlChanged Url
+
+
+type alias Model =
+    { navKey : Nav.Key
+    , page : Page
+    , posts : List BlogPost
+    }
+
+
+type Page
+    = PostListing
+    | PostView BlogPost
+    | NotFound
 
 
 type alias BlogPost =
@@ -10,15 +31,122 @@ type alias BlogPost =
     , body : String
     }
 
+
+type alias Flags =
+     ()
+
+
+main : Program Flags Model Msg
 main =
-    notFoundView
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = always Sub.none
+        , onUrlRequest = UrlRequested
+        , onUrlChange = UrlChanged
+        }
 
 
-notFoundView : Html msg
+init : Flags -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url key =
+   ( { navKey = key
+     , page = PostListing
+     , posts = initPosts
+     }
+   , Cmd.none
+   )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        UrlRequested req ->
+            case req of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.navKey (Url.toString url) )
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | page = getPage (fragmentToPath url.fragment) model }, Cmd.none )
+
+
+fragmentToPath : Maybe String -> List String
+fragmentToPath frag =
+    case frag of
+        Just str ->
+            String.split "/" str
+
+        Nothing ->
+            []
+
+
+getPage : List String -> Model -> Page
+getPage path model =
+    case path of
+        [ "blog" ] ->
+            PostListing
+
+        [ "blog", slug ] ->
+            List.filter (\post -> post.slug == slug) model.posts
+                |> List.head
+                |> Maybe.map PostView
+                |> Maybe.withDefault NotFound
+
+        _ ->
+            NotFound
+
+
+renderPage : Model -> Html Msg
+renderPage model =
+    case model.page of
+        PostListing ->
+            listView model.posts
+
+        PostView post ->
+            postView post
+
+        NotFound ->
+            notFoundView
+
+
+view : Model -> Document Msg
+view model =
+    { title = "The Blog"
+    , body =
+        [ a [ href "#blog" ] [ text " Home " ]
+        , renderPage model
+        ]
+    }
+
+
+notFoundView : Html Msg
 notFoundView =
     div
         []
         [ h1 [] [ text "404 Not Found" ]
+        ]
+
+
+listView : List BlogPost -> Html Msg
+listView posts =
+    let
+        renderPost post =
+            div
+                []
+                [ a [ href ("#blog/" ++ post.slug) ] [ text post.heading ]
+                ]
+    in
+        div [ style "margin" "2rem" ] (List.map renderPost posts)
+
+
+postView : BlogPost -> Html Msg
+postView post =
+    div
+        []
+        [ h1 [] [ text post.heading ]
+        , Html.p [] [ text post.body ] 
         ]
 
 
